d70ca6de1e6bd20e4f792c5dbb934dbcc95438d6
Tauhid <tawhidiam1@gmail.com>
2024-08-21 22:49:14 +0600

    second commit

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&y)fkt=izq330!azwbgx^-bm=+bn3n2n*b6m^$nsg1^+2=wixi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
